5b8462c7c07fd65d8c80155ae4e5281bec6fc749
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-11 13:08:51 +0330

    project init

---
+++ pages/products/[id].js
@@ -0,0 +1,53 @@
+import "@/styles/Product.module.css";
+
+import Comments from "@/components/templates/Product/Comments";
+import ProductsDetails from "@/components/templates/Product/ProductDetails";
+
+const Product = ({ product, comments }) => {
+  return (
+    <>
+      <ProductsDetails data={product} />
+      <Comments data={comments} />
+    </>
+  );
+};
+
+export async function getStaticPaths(context) {
+  const res = await fetch(`http://localhost:4000/menu`);
+  const products = await res.json();
+
+  const paths = products.map((product) => ({
+    params: { id: String(product.id) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productResponse = await fetch(
+    `http://localhost:4000/menu/${params.id}`
+  );
+  const productData = await productResponse.json();
+
+  const commentsResponse = await fetch(`http://localhost:4000/comments`);
+  const comments = await commentsResponse.json();
+
+  const productComments = comments.filter(
+    (comment) => comment.productID === +params.id
+  );
+
+  return {
+    props: {
+      product: productData,
+      comments: productComments,
+    },
+    revalidate: 60 * 60 * 12, // Second
+  };
+}
+
+export default Product;
